df752b569f12a1730844a40d578e9dbfbd5877a5
alec.barns-graham <alec.barns-graham@featurespace.co.uk>
2021-02-09 22:16:33 +0000

    Add database module

---
+++ modules/rds_databases/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+provider "aws" {
+  alias  = "replica_region"
+  region = var.aws_back_up_region
+}
+
+resource "aws_db_subnet_group" "subnet_group" {
+  name       = "main_subnet_group"
+  subnet_ids = [var.subnet_1, var.subnet_2, var.subnet_3]
+}
+
+resource "aws_db_instance" "s3_key_database" {
+  provider                = aws
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "postgres"
+  instance_class          = "db.t2.micro"
+  name                    = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+  tags                    = var.tags
+  skip_final_snapshot     = true
+  backup_retention_period = 30
+  vpc_security_group_ids  = [var.security_group_id]
+  db_subnet_group_name    = aws_db_subnet_group.subnet_group.name
+}
+
+resource "aws_db_instance" "read_replica_database" {
+  provider                = aws
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "postgres"
+  instance_class          = "db.t2.micro"
+  name                    = var.db_name
+  username                = var.db_username
+  replicate_source_db     = aws_db_instance.s3_key_database.arn
+  tags                    = var.tags
+  vpc_security_group_ids  = [var.security_group_id]
+  skip_final_snapshot     = true
+}
+
+resource "aws_db_instance" "read_replica_other_region_database" {
+  provider                = aws.replica_region
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "postgres"
+  instance_class          = "db.t2.micro"
+  name                    = var.db_name
+  username                = var.db_username
+  replicate_source_db     = aws_db_instance.s3_key_database.arn
+  tags                    = var.tags
+  skip_final_snapshot     = true
+}
