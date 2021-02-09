e67a07c496833e2dcae6b1d03ef6add9d42d871f
alec.barns-graham <alec.barns-graham@featurespace.co.uk>
2021-02-09 22:15:48 +0000

    Add domain name module

---
+++ modules/domain_name/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+resource "aws_route53_record" "api_record" {
+  zone_id = var.hosted_zone_id
+  name    = "api.${var.domain_name}"
+  type    = "A"
+  alias {
+    name                   = var.load_balancer_domain_name
+    zone_id                = var.load_balancer_zone_id
+    evaluate_target_health = true
+  }
+}
